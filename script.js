2270d6ecba378c0a9c766b11a141560246ec71ad
a-korell <adrianokorell@hotmail.com>
2020-05-09 22:39:45 -0300

    Add movement to snake

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function createBG() {
     context.fillStyle = "#263445";
@@ -19,5 +20,27 @@ function createSnake(){
     }
 }
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
+
